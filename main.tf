dcb7cb64c8cb99330f8d18787cf4e096ee024acb
sriteja_nemani <sriteja@stratosfy.io>
2026-09-11 18:10:10 -0400

    terraform

--- main.tf
+++ main.tf
@@ -95,3 +95,51 @@ resource "aws_route_table" "database" {
         }
     )
 }
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "nat" {
+  domain                    = "vpc"
+  tags = merge(
+        local.common_tags,
+        var.eip_tags,
+        {
+          Name = "${var.project}-${var.environment}-nat"
+        }
+    )
+}
+
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id    #we are slecting one of two available az's   #NAT is placed in PUBLIC to communicate (eggress only) with 0.0.0.0 
+
+  tags = merge(
+        local.common_tags,
+        var.nat_gateway_tags,
+        {
+          Name = "${var.project}-${var.environment}"
+        }
+    )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
